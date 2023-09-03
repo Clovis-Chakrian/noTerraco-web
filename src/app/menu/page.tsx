259ee7d3180eb741038d4b5ae0cdf77eb29593c5
Clóvis Chakrian <clovischakriandev@gmail.com>
2023-09-03 20:09:06 -0300

    updated the packages

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -4,9 +4,22 @@ import styles from './page.module.css';
 
 export const revalidate = 10;
 
+async function getData() {
+  const res = await fetch('https://noterracorestaurante.herokuapp.com/products')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+
 export default async function Menu() {
-  const response = await fetch('https://noterracorestaurante.herokuapp.com/products');
-  const products: IProduct[] = await response.json();
+  //const response = await fetch('https://noterracorestaurante.herokuapp.com/products');
+  const products: IProduct[] = await getData();
 
   return (
     <>
