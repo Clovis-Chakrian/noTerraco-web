794450e71a94d8eef68a36322d2c870edd83e74f
Clóvis Chakrian <clovischakriandev@gmail.com>
2023-09-03 19:59:23 -0300

    updated the packages

--- next.config.js
+++ next.config.js
@@ -8,6 +8,13 @@ const ContentSecurityPolicy = `
 `;
 
 const nextConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    })
+    return config
+  },
   experimental: {
     appDir: true,
   },
